91a3b7f1e6e270f268a142fdfe497ec67252401b
justin <just.mimo15@gmail.com>
2024-03-25 19:47:21 -0400

    ajout de fonctions

--- traitement_image.py
+++ traitement_image.py
@@ -1,3 +1,4 @@
+from PIL import Image
 def appliquer_rgb_to_gry(chemin_image_couleur,chemin_sauvegarde_gris):
 
     img_gris = []
@@ -6,12 +7,9 @@ def appliquer_rgb_to_gry(chemin_image_couleur,chemin_sauvegarde_gris):
 
         # calcul le niveaux de gris pour chaque pixel
         for i in range(len(str_couleur)):
-            for j in range(len(str_couleur[0])):
-                r = str_couleur[j]
-                g = str_couleur[j+1]
-                b = str_couleur[j+2]
+                r, g, b = str_couleur[i][0], str_couleur[i][1], str_couleur[i][2]
                 niveau_gris = (r+g+b) // 3
-                img_gris[i][j] = niveau_gris
+                img_gris.append(niveau_gris)
                 #Reset la moyenne
                 moyenne = 0
     with open(chemin_sauvegarde_gris, 'w') as image_grise:

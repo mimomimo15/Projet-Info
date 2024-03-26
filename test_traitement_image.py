6af9b706b37152bad81c440641a10d6ad0ac47e1
justin <just.mimo15@gmail.com>
2024-03-26 16:42:28 -0400

    ajout de fonctions

--- test_traitement_image.py
+++ test_traitement_image.py
@@ -17,3 +17,53 @@ def test_appliquer_transformation_1_a():
     ])
 
     assert np.array_equal(appliquer_transformation_1(tableau), tableau_attendu)
+
+def test_appliquer_transformation_1_b():
+
+    tableau = np.array([
+        [80, 20, 56, 49, 145, 123],
+        [60, 17, 99, 121, 55, 56],
+        [80, 8, 45, 100, 99, 30],
+        [255, 23, 155, 88, 12, 78],
+        [100, 200, 23, 82, 155, 254]
+    ])
+    tableau_attendu = np.array([
+        [0, 0, 0, 0, 0, 0],
+        [0, 251, 24, 32, 119, 0],
+        [0, 255, 124, 66, 129, 0],
+        [0, 191, 2, 105, 255, 0],
+        [0, 0, 0, 0, 0, 0]
+    ])
+
+    assert np.array_equal(appliquer_transformation_1(tableau), tableau_attendu)
+
+
+def test_appliquer_transformation_2_a():
+
+    tableau = np.array([[88, 102, 133, 49, 145, 123],
+                        [14, 100, 200, 121, 55, 56],
+                        [40, 101, 92, 100, 99, 30],
+                        [255, 23, 155, 88, 12, 78],
+                        [100, 200, 23, 82, 155, 254]])
+
+    tableau_attendu = np.array([[0, 0, 0, 0, 0, 0],
+                                [0, 3, 6, 3, 6, 0],
+                                [0, 6, 5, 3, 5, 0],
+                                [0, 7, 5, 3, 6, 0],
+                                [0, 0, 0, 0, 0, 0]])
+    assert np.array_equal(appliquer_transformation_2(tableau, 1), tableau_attendu)
+
+def test_appliquer_transformation_2_b():
+
+    tableau = np.array([[88, 102, 133, 49, 145, 123],
+                        [14, 100, 200, 121, 55, 56],
+                        [40, 101, 92, 100, 99, 30],
+                        [255, 23, 155, 88, 12, 78],
+                        [100, 200, 23, 82, 155, 254]])
+
+    tableau_attendu = np.array([[0, 0, 0, 0, 0, 0],
+                                [0, 0, 0, 0, 0, 0],
+                                [0, 0, 4, 5, 0, 0],
+                                [0, 0, 0, 0, 0, 0],
+                                [0, 0, 0, 0, 0, 0]])
+    assert np.array_equal(appliquer_transformation_2(tableau, 2), tableau_attendu)
--- traitement_image.py
+++ traitement_image.py
@@ -1,5 +1,9 @@
 from PIL import Image
 import numpy as np
+from math import log10
+
+#  ____________CORRIGER LES TABLEAUX LES UTILISER ET RETOURNER EN NUMPY__________________
+
 def appliquer_rgb_to_gry(chemin_image_couleur,chemin_sauvegarde_gris):
 
     img_gris = []
@@ -18,34 +22,43 @@ def appliquer_rgb_to_gry(chemin_image_couleur,chemin_sauvegarde_gris):
 
 def appliquer_transformation_1(tab_gris):
 
+    #Création du tableau transformé de dimensions identique au tableau recus
     tab_trans = np.zeros(shape = (len(tab_gris), len(tab_gris[0])))
-    print(tab_gris)
-
-
-
-    # i et x == lignes
-    # j et y == colonne
+    # La première boucle positionne le gc
     for i in range(1, len(tab_gris)-1):
         for j in range(1, len(tab_gris[0])-1):
             gc = tab_gris[i][j]
-            print(gc)
+
+            # Énumeration et création du tableau des voisins du centre afin qu'ils soit dans le bon ordre
+            v0 = tab_gris[i-1][j-1]
+            v1 = tab_gris[i-1][j]
+            v2 = tab_gris[i-1][j+1]
+            v3 = tab_gris[i][j+1]
+            v4 = tab_gris[i+1][j+1]
+            v5 = tab_gris[i+1][j]
+            v6 = tab_gris[i+1][j-1]
+            v7 = tab_gris[i][j-1]
+            #le tableau de voisin represente le parcours que doit faire la boucle, (matrice 3x3 sans le centre)
+            tab_voisin = [v0, v1, v2, v3, v4, v5, v6, v7]
+            # Initialisation des données nécéssaire dans la prochaine boucle
             binaire = []
             res = 0
+            multi = 0
 
-            for x in range(i-1, i+2):
-                for y in range(j-1, j+2):
-                    if (x, y) != (i, j):
-                        gv = tab_gris[x][y]
-                        if gv-gc >= 0:
-                            binaire.append(1)
-                        else:
-                            binaire.append(0)
-            print(binaire)
+            # Boucle qui parcours le tableau de voisin et qui crée ensuite le nombre binaire correspondant au centre
+            for x in range(len(tab_voisin)):
+                if tab_voisin[x] >= gc:
+                    binaire.append(1)
+                else:
+                    binaire.append(0)
+            #Boucle qui parcour la liste binaire à l'envers pour ensuite le transformer en décimal
+            for y in range(len(binaire)-1, -1, -1):
+                res += binaire[y] * (2**multi)
+                multi += 1
 
-
-            for r in range(len(binaire)):
-                res += binaire[r] * (2**r)
+            # intergrer les nombres décimal calculé dans le tableaux transformé
             tab_trans[i][j] = res
+
 
     # Mettre les colones et range exterieur a 0
     tab_trans[0, :] = 0
@@ -53,9 +66,37 @@ def appliquer_transformation_1(tab_gris):
     tab_trans[:, 0] = 0
     tab_trans[:, -1] = 0
 
-    print(tab_trans)
+    return tab_trans
+
+def appliquer_transformation_2(tab_gris, r):
+
+    # Création du tableau transformé de dimensions identique au tableau recus
+    tab_trans = np.zeros(shape=(len(tab_gris), len(tab_gris[0])))
+
+    # La première boucle positionne I(x,y)
+    for x in range(r, len(tab_gris) - r):
+        for y in range(r, len(tab_gris[0]) - r):
+
+            #Calcul des valeurs intermediaires
+            v1 = 1 + np.abs(tab_gris[x][y + r] - 2 * tab_gris[x][y] + tab_gris[x][y-r])
+            v2 = 1 + np.abs(tab_gris[x+r][y] - 2 * tab_gris[x][y] + tab_gris[x-r][y])
+            v3 = 1 + np.abs(tab_gris[x-r][y+r] - 2 * tab_gris[x][y] + tab_gris[x+r][y-r])
+
+            #Calcul de la valeur final et le // 1 sert a garder seulement l'entier
+            o_xy = (np.log10(v1) + np.log10(v2) + np.log10(v3))//1
+            tab_trans[x][y] = o_xy
+
+    # Mettre les colones et range exterieur a 0
+    tab_trans[0, :] = 0
+    tab_trans[-1, :] = 0
+    tab_trans[:, 0] = 0
+    tab_trans[:, -1] = 0
 
     return tab_trans
+
+
+
+
 
 
 
